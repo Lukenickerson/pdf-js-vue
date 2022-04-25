ab147f9396b8922331fcaba59f7343e0bbdf8027
Luke Nickerson <lnickerson@itsmr.org>
2022-04-25 17:17:41 -0400

    Added option to add mount element automatically

--- src/pdf-js-vue.js
+++ src/pdf-js-vue.js
@@ -1,9 +1,11 @@
 import pdfOverlayViewerComponent from './pjvOverlayViewerComponent.js';
 
 function addViewerApp(options = {}) {
-	const { vue, mountId, pdfJsWorkerSrc, pdfDocumentUrl } = options;
+	const { vue, mountId, pdfJsWorkerSrc, pdfDocumentUrl, addMountElement } = options;
 	if (!vue) throw new Error('vue is required');
 	if (!mountId) throw new Error('mountId is required');
+	if (!pdfJsWorkerSrc) console.warn('No pdfJsWorkerSrc param was provided. This will likely cause an error when opening PDFs.');
+	if (!pdfDocumentUrl) console.warn('No pdfDocumentUrl param was provided.');
 	const appOptions = {
 		components: {
 			'pdf-overlay-viewer-component': pdfOverlayViewerComponent,
@@ -21,6 +23,13 @@ function addViewerApp(options = {}) {
 		}
 	};
 	const app = vue.createApp(appOptions);
+	// If the mount/container element doesn't exist, let's create it at the end of the body
+	if (addMountElement && window && window.document && !window.document.getElementById(mountId)) {
+		const mountElt = window.document.createElement('div');
+		mountElt.id = mountId;
+		const body = window.document.getElementsByTagName('body')[0];
+		body.appendChild(mountElt);
+	}
 	app.mount(`#${mountId}`);
 }
 
